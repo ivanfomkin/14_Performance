4b2b53018277fe70a6b92194696600f44a5ae3bd
Ivan <ivan-fomkin@yandex.ru>
2020-04-23 21:39:05 +0300

    Increase application speed

--- src/thirdTask/Loader.java
+++ src/thirdTask/Loader.java
@@ -4,6 +4,8 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 
+import static thirdTask.XMLHandler.voters;
+
 public class Loader {
     //Сократим класс Loader до нескольких строчек кода. Для вставки в БД нам будет достаточно следующего кода:
     public static void main(String[] args) throws Exception {
@@ -14,6 +16,13 @@ public class Loader {
         SAXParser parser = factory.newSAXParser();
         XMLHandler handler = new XMLHandler();
         parser.parse(new File(fileName), handler);
+        voters.forEach((n, b) -> {
+            try {
+                DBConnection.countVoter(n, b);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
         DBConnection.executeMultiInsert(); //Выполним insert того, что осталось в StringBuilder
         System.out.println("Time: " + (System.currentTimeMillis() - start) / 1000 / 60 + " minutes");
     }
--- src/thirdTask/XMLHandler.java
+++ src/thirdTask/XMLHandler.java
@@ -3,17 +3,21 @@ package thirdTask;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Немнго оптимизируем класс XMLHandler, теперь он только вызывает метод countVoter в классе
  * DBConnection. Это сделано для простоты и снижения времени работы программы.
  */
 public class XMLHandler extends DefaultHandler {
+    protected static Map<String, String> voters = new HashMap<>();
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
         try {
             if (qName.equals("voter")) {
-                DBConnection.countVoter(attributes.getValue("name"), attributes.getValue("birthDay"));
+                voters.put(attributes.getValue("name"), attributes.getValue("birthDay"));
             }
         } catch (Exception e) {
             e.printStackTrace();
